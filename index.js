6726ad53378be4a924936770f8fa412cc2ae616c
Huynhthienloc <99669216+Huynhthienloc@users.noreply.github.com>
2022-03-09 15:24:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-function randomNumber1() {
-	var randomNumber1 = Math.floor(Math.random() * 6) + 1
-	var randomImg = "dice" + randomNumber1 +".png"
-	var randomSource = "images/" + randomImg
-	return randomSource
-}
-document.querySelector(".img1").setAttribute("src", randomNumber1())
 
-function randomNumber2() {
-	var randomNumber2 = Math.floor(Math.random() * 6) + 1 
-	var randomImg = "dice" + randomNumber2 +".png"
-	var randomSource = "images/" + randomImg
-	return randomSource
-}
+var randomNumber1 = Math.floor(Math.random() * 6) + 1
+var randomImg1 = "dice" + randomNumber1 +".png"
+var randomSource1 = "images/" + randomImg1
+document.querySelector(".img1").setAttribute("src", randomSource1)
 
-document.querySelector(".img2").setAttribute("src", randomNumber2())
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 
+var randomImg2 = "dice" + randomNumber2 +".png"
+var randomSource2 = "images/" + randomImg2
+document.querySelector(".img2").setAttribute("src", randomSource2 )
 
 if (randomNumber1 > randomNumber2){
 	document.querySelector(".heading").innerHTML = "Player 1 win" 
